85773f850b00966cbe3ead87a86b6f8d0f1c8571
7d <61975820+iiiii7d@users.noreply.github.com>
2022-10-09 14:17:15 +0800

    get_flight_route

--- mrt-flightradar-common/src/flight_route/mod.rs
+++ mrt-flightradar-common/src/flight_route/mod.rs
@@ -1,4 +1,28 @@
+use anyhow::Result;
+
+use crate::{
+    data_types::{airport::Runway, vec::FromLoc},
+    flight_route::{
+        flight_path::get_flight_path, types::path::Path, waypoint_route::get_waypoint_route,
+    },
+};
+
 mod between_waypoints;
 mod flight_path;
 pub mod types;
 mod waypoint_route;
+
+pub fn get_flight_route(start_runway: &Runway, end_runway: &Runway) -> Result<Vec<Path>> {
+    let start_vec = FromLoc {
+        tail: start_runway.vec.tail,
+        vec: start_runway.vec.vec.normalize() * (500.0 + start_runway.vec.vec.length()),
+    };
+    let end_vec_vec = end_runway.vec.vec.normalize() * (500.0 + end_runway.vec.vec.length());
+    let end_vec = FromLoc {
+        tail: end_runway.vec.head() - end_vec_vec,
+        vec: end_vec_vec,
+    };
+
+    let waypoints = get_waypoint_route(start_vec, end_vec)?;
+    Ok(get_flight_path(start_vec, end_vec, waypoints, 100.0))
+}
