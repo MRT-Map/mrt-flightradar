a135a760d3621a3e031ba04611f23516800fa7d0
7d <61975820+iiiii7d@users.noreply.github.com>
2022-10-10 01:51:49 +0800

    start of server w/ rocket

--- mrt-flightradar-server/src/main.rs
+++ mrt-flightradar-server/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use rocket::routes;
+
+#[rocket::get("/")]
+fn test() -> &'static str {
+    "abc"
+}
+
+#[rocket::main]
+async fn main() -> Result<()> {
+    let _ = rocket::build().mount("/", routes![test]).launch().await?;
+    Ok(())
 }
