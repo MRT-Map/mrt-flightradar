1c34f689a0f564e23dd99e0106960923dd353269
7d <61975820+iiiii7d@users.noreply.github.com>
2022-10-10 13:08:16 +0800

    flight generation

---
+++ mrt-flightradar-server/src/flight_generation.rs
@@ -0,0 +1,65 @@
+use anyhow::{anyhow, Result};
+use common::{
+    data_types::{airport::AirFacility, RAW_DATA},
+    flight_route::get_flight_route,
+};
+use rand::{prelude::SliceRandom, Rng};
+use tokio::time::Instant;
+
+use crate::{ActiveFlight, ActiveFlightInfo, FLIGHTS};
+
+const AIRLINE_NAMES: [&str; 6] = [
+    "Example Air",
+    "Lorem Ipsum Air",
+    "Broken GPS Airlines",
+    "0% Reliability Airlines",
+    "Gas",
+    "Lumeva Airlink",
+];
+
+const AIRCRAFT_NAMES: [&str; 4] = ["Stratus SA-1", "IntraJet ExpiXS", "Fighter Jet", "Dragon"];
+
+pub async fn flight_generation() -> Result<()> {
+    let mut new_flights = vec![];
+    let num_new_flights = rand::thread_rng().gen_range(0..5);
+    for _ in 0..num_new_flights {
+        let airports = RAW_DATA
+            .air_facilities
+            .iter()
+            .filter_map(|af| {
+                if let AirFacility::Airport { code, ty, runways } = &af {
+                    Some((code, ty, runways))
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>();
+        let ((airport1, _, runways1), (airport2, _, runways2)) = {
+            let chosen = airports
+                .choose_multiple(&mut rand::thread_rng(), 2)
+                .collect::<Vec<_>>();
+            (chosen[0], chosen[1])
+        };
+        let (runway1, runway2) = (
+            runways1
+                .choose(&mut rand::thread_rng())
+                .ok_or_else(|| anyhow!("No runways"))?,
+            runways2
+                .choose(&mut rand::thread_rng())
+                .ok_or_else(|| anyhow!("No runways"))?,
+        );
+        new_flights.push(ActiveFlight {
+            route: get_flight_route(runway1, runway2)?,
+            depart_time: Instant::now(),
+            info: ActiveFlightInfo {
+                airline_name: AIRLINE_NAMES.choose(&mut rand::thread_rng()).unwrap(),
+                aircraft: AIRCRAFT_NAMES.choose(&mut rand::thread_rng()).unwrap(),
+                registry_code: "".into(),
+                from: airport1,
+                to: airport2,
+            },
+        })
+    }
+    FLIGHTS.lock().await.append(&mut new_flights);
+    Ok(())
+}
--- mrt-flightradar-server/src/main.rs
+++ mrt-flightradar-server/src/main.rs
@@ -1,5 +1,34 @@
+mod flight_generation;
+
+use std::{sync::Arc, time::Duration};
+
 use anyhow::Result;
+use common::{data_types::timetable::AirportCode, flight_route::types::path::FlightPath};
+use once_cell::sync::Lazy;
 use rocket::routes;
+use smol_str::SmolStr;
+use tokio::{sync::Mutex, time::Instant};
+
+use crate::flight_generation::flight_generation;
+
+#[derive(Clone, Debug)]
+pub struct ActiveFlightInfo<'a> {
+    pub airline_name: &'a str,
+    pub aircraft: &'a str,
+    pub registry_code: SmolStr,
+    pub from: &'a AirportCode,
+    pub to: &'a AirportCode,
+}
+
+#[derive(Clone, Debug)]
+pub struct ActiveFlight<'a> {
+    pub route: FlightPath,
+    pub depart_time: Instant,
+    pub info: ActiveFlightInfo<'a>,
+}
+
+static FLIGHTS: Lazy<Arc<Mutex<Vec<ActiveFlight>>>> =
+    Lazy::new(|| Arc::new(Mutex::new(Vec::new())));
 
 #[rocket::get("/")]
 fn test() -> &'static str {
@@ -8,6 +37,12 @@ fn test() -> &'static str {
 
 #[rocket::main]
 async fn main() -> Result<()> {
+    tokio::spawn(async {
+        loop {
+            flight_generation().await.unwrap();
+            tokio::time::sleep(Duration::from_secs(30)).await;
+        }
+    });
     let _ = rocket::build().mount("/", routes![test]).launch().await?;
     Ok(())
 }
