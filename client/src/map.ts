b457695b566556c41cce5c08b3ffcebd8874d22f
7d <61975820+iiiii7d@users.noreply.github.com>
2023-10-22 13:30:21 +0800

    fix map tiles

--- client/src/map.ts
+++ client/src/map.ts
@@ -36,7 +36,7 @@ L.TileLayer.customTileLayer = L.TileLayer.extend({
 
     if (zzz.length != 0) zzz += "_";
 
-    let url = `https://dynmap.minecartrapidtransit.net/tiles/new/flat/${group.x}_${group.y}/${zzz}${numberInGroup.x}_${numberInGroup.y}.png`;
+    let url = `https://dynmap.minecartrapidtransit.net/main/tiles/new/flat/${group.x}_${group.y}/${zzz}${numberInGroup.x}_${numberInGroup.y}.png`;
     //console.log(url)
     return url;
 
